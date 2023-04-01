c7482fa3c76fc6b79a3dc37e80b33b5fae5ece5a
Kiangebenimatias <129292053+Kiangebenimatias@users.noreply.github.com>
2023-04-01 12:29:14 +0100

    atualizei mas

---
+++ abrir/abrirconta.js
@@ -0,0 +1,37 @@
+function cria() {
+    var nome = document.getElementById("txtabrirnome")
+    var apelido = document.getElementById("txtabrirapelido")
+    var telefone = document.getElementById("txtabrirtel")
+    var data = document.getElementById("txtabrirdata")
+    var email = document.getElementById("txtabriremail")
+    var morada = document.getElementById("txtabrirmorada")
+    var palavrapasse1 = document.getElementById("txtabrirpalavrapasse1")
+    var palavrapasse2 = document.getElementById("txtabrirpalavrapasse2")
+    var codigo = document.getElementById("txtabrircodigo")
+    if (nome.value.length == 0 || apelido.value.length == 0 || telefone.value.length == 0 || data.value.length == 0 || email.value.length == 0 || morada.value.length == 0 || palavrapasse1.value.length == 0 || palavrapasse2.value.length == 0 || codigo.value.length == 0 || nome.value.length == 0 ) {
+        Swal.fire({
+            title: 'Atenção!',
+            text: 'Preecha bem os dados acima',
+            icon: 'error',
+            confirmButtonText: 'Repetir'
+          })
+        nome.style.background = "red"
+        apelido.style.background = "red"
+        telefone.style.background = "red"
+        data.style.background = "red"
+        email.style.background = "red"
+        morada.style.background = "red"
+        palavrapasse1.style.background = "red"
+        palavrapasse2.style.background = "red"
+        codigo.style.background = "red"
+    } else if (palavrapasse1 == palavrapasse2) {
+        
+    } else {
+        Swal.fire({
+            title: 'Ben vindo a EssênciMenSager',
+            text: 'A sua conta foi criada com sucesso',
+            icon: 'success',
+            confirmButtonText: 'Confirmar!'
+          })
+    }
+}
